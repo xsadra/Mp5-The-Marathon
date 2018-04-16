22c01cb5b3ab0487a09c40002eff02c4e0244b2f
Sadra <ti9mail@gmail.com>
2018-04-16 08:23:59 +0200

    Add Spring structure

---
+++ src/main/java/de/sadrab/Mp5TheMarathon/Mp5TheMarathonApplication.java
@@ -0,0 +1,12 @@
+package de.sadrab.Mp5TheMarathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp5TheMarathonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mp5TheMarathonApplication.class, args);
+	}
+}
